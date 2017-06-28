84377109868a7a3bd570ad87c77b9c398ab72f4b
Finlay McAfee <finlay.mcafee@skyscanner.net>
2017-06-28 23:22:20 +0100

    Initial Commit. Rough opening image

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import FinlayMcAfee from './FinlayMcAfee';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <FinlayMcAfee/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/BackgroundImage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './BackgroundImage.css';
+
+const BackgroundImage = () => (
+  <div className="background-image"/>
+);
+
+export default BackgroundImage;
