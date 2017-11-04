df08cd817c3767d88eabce207768cab8eb8c4b32
Finlay McAfee <finlay.mcafee@skyscanner.net>
2017-11-04 11:42:09 +0000

    Basic Bio Page added with lorem

--- src/BioPage.js
+++ src/BioPage.js
@@ -1,18 +1,22 @@
 import React from 'react'
+import './BioPage.css'
 
 const BioPage = () => (
-  <div>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Donec mollis pellentesque est, et lacinia arcu feugiat at.
-      Aenean tincidunt orci at eros faucibus rutrum.
-      Aenean sagittis ante sed massa euismod blandit.
-      Etiam tempor at turpis id sagittis.
-      Donec eu elit justo. Morbi sagittis tempor tincidunt.
-      Donec ornare nisl et vulputate laoreet.
-      Donec nec urna nibh. Curabitur quis efficitur augue.
-      Quisque id sagittis ex.
-    </p>
+  <div className='container'>
+    <div id='headshot-left' />
+    <div id='text-right'>
+      <p id='bio-text'>
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+         Donec mollis pellentesque est, et lacinia arcu feugiat at.
+         Aenean tincidunt orci at eros faucibus rutrum.
+         Aenean sagittis ante sed massa euismod blandit.
+         Etiam tempor at turpis id sagittis.
+         Donec eu elit justo. Morbi sagittis tempor tincidunt.
+         Donec ornare nisl et vulputate laoreet.
+         Donec nec urna nibh. Curabitur quis efficitur augue.
+         Quisque id sagittis ex.
+      </p>
+    </div>
   </div>
 )
 
--- src/FinlayMcAfee.js
+++ src/FinlayMcAfee.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import finlaymcafee from './FinlayMcAfee.png';
-import './FinlayMcAfee.css';
+import React from 'react'
+import finlaymcafee from './FinlayMcAfee.png'
+import './FinlayMcAfee.css'
 
 const FinlayMcAfee = () => (
-  <div className="name-container">
-    <img className="finlay-mcafee" src={finlaymcafee} alt="Name"/>
+  <div className='name-container'>
+    <img className='finlay-mcafee' src={finlaymcafee} alt='Name' />
   </div>
-);
+)
 
-export default FinlayMcAfee;
+export default FinlayMcAfee
